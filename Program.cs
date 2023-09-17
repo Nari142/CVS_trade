aa58397e6a704541da8d3cb0e2ae26feca63e75b
Oleg <yudnikovo@bk.ru>
2023-09-18 00:04:31 +0700

    commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          string connStr = "server=localhost;user id=root;password=Ab_42211;database=CVS_trade;";
+          MySqlConnection conn = new MySqlConnection(connStr);  
+
+          conn.Open();
+          Console.WriteLine("Open");
+          //Create_tab(conn);
+          CVS(conn);
+          conn.Close();
+          Console.WriteLine("Close");
+        }
+
+        public static void Create_tab(MySqlConnection conn){
+          string sql = "CREATE TABLE trade( category VARCHAR(250), code VARCHAR(250), name VARCHAR(250), owner VARCHAR(250));";
+          MySqlCommand command = new MySqlCommand(sql, conn);
+          command.ExecuteNonQuery();
+        }
+
+        public static void CVS (MySqlConnection conn){
+          string sql = "LOAD DATA INFILE 'data.CSV' INTO TABLE trade FIELDS TERMINATED BY ',' IGNORE 1 ROWS;";
+          MySqlCommand command = new MySqlCommand(sql, conn);
+          command.ExecuteNonQuery();
+        }
+    }
+}
---
+++ obj/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+ 
+using MySql.Data.MySqlClient;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string args)
+        {
+          string connStr = "server=localhost;user=root;database=CVS_trade;";
+          MySqlConnection conn = new MySqlConnection(connStr);  
+          conn.Open();
+          Console.WriteLine("Open");
+          string sql = "CREATE TABLE trade( category VARCHAR(250), code VARCHAR(250), name VARCHAR(250), owner VARCHAR(250));";
+          MySqlCommand command = new MySqlCommand(sql, conn);
+          conn.Close();
+          Console.WriteLine("Close");
+        }
+    }
+}
